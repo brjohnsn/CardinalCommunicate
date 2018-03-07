77546611688156a9d3eead2f4d19569bbca1ff1d
Noah Shirey <nishirey@bsu.edu>
2018-03-07 11:56:11 -0500

    begin style welcome component

--- frontend/src/Components/Home.js
+++ frontend/src/Components/Home.js
@@ -8,7 +8,11 @@ export default class Home extends Component{
 
     render(){
         return(
-            <div>Hey</div>
+            <div>
+                <h1 style={{textAlign: 'center'}}>Welcome!</h1>
+                <p style={{width: "25vw"}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque nunc lacus, sodales et justo ut, tempor rutrum sapien. Fusce pellentesque sem at egestas tincidunt. Integer rhoncus semper augue vel maximus. Duis a elit est. Sed convallis tincidunt mauris, et ultrices purus tincidunt eget. Nulla facilisi. In euismod gravida lacinia. Curabitur non neque id orci tristique pulvinar. Etiam cursus ante auctor nibh porta lobortis.
+                </p>
+            </div>
         );
     }
 }
