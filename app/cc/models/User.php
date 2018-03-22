01018153970782ed770062fffcbd2576c8e075c0
Chris Savage <cesavage@bsu.edu>
2018-03-21 21:01:37 -0400

    Begin refactoring test unit tests and User class.

--- app/cc/models/User.php
+++ app/cc/models/User.php
@@ -5,72 +5,20 @@ use cc\models\Password;
 
 //require "../../../utilities/Database.php";
 
-class User {
+class User
+{
 
     private $username;
     private $email;
     private $password;
     private $salt;
 
-    public function __construct($request){
-        $userAttributes = $request->getParsedBody();
-        if (isset($userAttributes['username'])){
-            $this->username = $userAttributes["username"];
-        }
-
-        if (isset($userAttributes["email"])){
-            $this->email = $userAttributes["email"];
-        }
-
-        if (isset($userAttributes["password"])){
-            $this->password = $userAttributes["password"];
-        }
-
-        if (isset($userAttributes["salt"])) {
-            $this->salt = $userAttributes["salt"];
+    public function __construct($userAttributes)
+    {
+        foreach ($userAttributes as $attributeName => $attributeValue){
+            ;
         }
     }
 
-
-
-    public function addToDatabase(){
-        if(!isset($this->salt)){
-            $this->salt = Password::generateEncryptionSalt(32);
-        }
-        $hashedPassword = Password::hash($this->password,$this->salt);
-        $database = Database::getDatabaseConnection();
-        $sql = "INSERT INTO users (username, email, password, salt) VALUES (?,?,?,?)";
-        $database->getQueryResult($sql,  array($this->username, $this->email, $hashedPassword, $this->salt));
-    }
-
-    public function signIn(){
-        $sql = "SELECT * FROM users WHERE username = ?";
-        $database = Database::getDatabaseConnection();
-        $result = $database->getQueryResult($sql, [$this->username,])->fetchObject();
-
-
-        $hashedPassword = Password::hash($this->password,$result->salt);
-
-        if($hashedPassword == $result->password){
-            $_SESSION['id'] = $result->id;
-            $_SESSION['loggedIn'] = true;
-            $_SESSION['username'] = $this->username;
-            $_SESSION['password'] = $this->password;
-            return true;
-        }
-
-        else{
-            session_destroy();
-            return false;
-        }
-
-    }
-    public static function getProfile($username){
-        $sql =  $sql = "SELECT * FROM users WHERE username = ?";;
-        $database = Database::getDatabaseConnection();
-        $result = $database->getQueryResult($sql, [$username])->fetchObject();
-        return $result;
-    }
 
 }
-?>
