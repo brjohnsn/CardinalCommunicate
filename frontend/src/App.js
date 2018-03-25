870c133d80b92de60971fb1fac13d263bd86bb8a
Noah Shirey <nishirey@bsu.edu>
2018-03-25 09:13:17 -0400

    add register component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import {Switch, Route} from 'react-router-dom';
 import Home from './Components/Home';
 import Login from './Components/Login';
 import Profile from './Components/Profile';
+import Register from './Components/Register';
 //import routes
 
 export default class App extends Component{
@@ -11,6 +12,7 @@ export default class App extends Component{
           <Switch>
               <Route exact path="/" component={Home} />
               <Route exact path="/Login" component={Login}/>
+              <Route exact path="/Register" component={Register}/>
               <Route exact path="/Profile" component={Profile}/>
           </Switch>
         );
