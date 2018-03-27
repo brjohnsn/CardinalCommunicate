a0b918d32cd3d3a123a366d9004f01000d769b0a
Noah Shirey <nishirey@bsu.edu>
2018-03-26 21:46:59 -0400

    Fix double bug where you had to double click radio tags to switch state and add flexbox styling

--- frontend/src/Components/Register.js
+++ frontend/src/Components/Register.js
@@ -16,23 +16,23 @@ export default class Register extends Component{
         }
     }
 
-
-    setGender(e){
-        e.preventDefault();
-        if(this.state.gender == 'male'){
-            document.getElementById('male').checked = false;
-            this.setState({gender:e.target.value})
-        }
-        if(this.state.gender == 'female'){
-            document.getElementById('female').checked = false;
-            this.setState({gender:e.target.value})
-
-        }
-        else{
-            this.setState({gender:e.target.value})
-        }
-
-    }
+    //
+    // setGender(e){
+    //     e.preventDefault();
+    //     if(this.state.gender == 'male'){
+    //         // document.getElementById('male').checked = false;
+    //
+    //     }
+    //     if(this.state.gender == 'female'){
+    //         // document.getElementById('female').checked = false;
+    //         this.setState({gender:e.target.value})
+    //
+    //     }
+    //     else{
+    //         this.setState({gender:e.target.value})
+    //     }
+    //
+    // }
 
     setInterpreter(e){
         e.preventDefault();
@@ -42,65 +42,54 @@ export default class Register extends Component{
 
     onSubmit(e){
         e.preventDefault();
-        axios.post("http://localhost:8888/CardinalCC/public/user/Register",{username:this.state.username, email:this.state.email, password:this.state.password, gender:this.state.gender, userStatus:this.state.userStatus}).then(()=> {
+        axios.post("http://localhost:8888/CardinalCC/public/user/Register",{username:this.state.username, email:this.state.email, password:this.state.password, gender:this.state.gender, userStatus:this.state.userStatus}).then((response)=> {
+            console.log(response.data)
             this.props.history.push("/Login")
         }
         )
 
     }
     render(){
-        let interpreterToggled = false;
         return(
-            <div style={{}}>
+            <div>
                 <form onSubmit={(e)=>this.onSubmit(e)} style={{marginLeft: "40%"}}>
                     <h1>Welcome! Register here!</h1>
 
                     <h2>Username</h2>
-                    <input onChange={(e)=>this.setState({username:e.target.value})} value={this.state.username}/>
+                    <input onChange={(e)=>this.setState({username:e.target.value})}/>
 
                     <h2>Email</h2>
-                    <input onChange={(e)=>this.setState({email:e.target.value})} value={this.state.email}/>
+                    <input onChange={(e)=>this.setState({email:e.target.value})}/>
 
                     <h2>Password</h2>
-                    <input onChange={(e)=>this.setState({password:e.target.value})} value={this.state.password}/>
+                    <input onChange={(e)=>this.setState({password:e.target.value})}/>
 
                     <h1>What is your gender?</h1>
-                    <div className={"genderInput"}>
-
-                        <div className={"male"}>
-                            <input type="radio" id="male" value="male" onChange={(e)=>{this.setGender(e)}}/>
-                            <label htmlfor="male">Male</label>
-                        </div>
-
-                        <div className={"female"}>
-                            <input type="radio" id="female" value="female" onChange={(e)=>{this.setGender(e)}}/>
-                            <label htmlfor="female">Female</label>
-                        </div>
-
+                    <div className={"genderInput"} style={{display:"flex", flexDirection: "column"}}>
+                        <input type="radio" name="gender" value="male" onChange={(e)=>{this.setState({gender:e.target.value})}}/>
+                        <label htmlfor="male">Male</label>
+                        <input type="radio" name="gender" value="female" onChange={(e)=>{this.setState({gender:e.target.value})}}/>
+                        <label htmlfor="female">Female</label>
                     </div>
 
                     <h1>Are you a client or an interpreter?</h1>
-                        <div className={"userTypeCheck"}>
-                            <div className={"deafUser"}>
-                                <input type="radio" id="deaf" value="Deaf" onChange={(e)=>{this.setState({userStatus: e.target.value})}}/>
+                        <div className={"userTypeCheck"} style={{display:"flex", flexDirection: "column"}}>
+                                <input type="radio" name="userType" value="Deaf" onChange={(e)=>{this.setState({userStatus: e.target.value})}}/>
                                 <label for="deaf">Deaf/Hard of Hearing</label>
-                            </div>
-
-                            <div className={"#"}>
-                                <input type="radio" id="other" value="other" onChange={(e)=>{this.setState({userStatus: e.target.value})}}/>
+                                <input type="radio" name="userType"  value="other" onChange={(e)=>{this.setState({userStatus: e.target.value})}}/>
                                 <label for="other">Other</label>
-                            </div>
-
-                            <div className={"interpreterUser"}>
-                                <input type="radio" id="interpreter" value="interpreter"  onChange={(e)=>{this.setInterpreter(e)}}/>
+                                <input type="radio" name="userType" value="interpreter"  onChange={(e)=>{this.setState({userStatus: e.target.value})}}/>
                                 <label for="interpreter">Interpreter</label>
-                            </div>
                             {
-                                this.state.userStatus == "interpreter" &&
-                                <div className="interpreterCertification">
+                                this.state.userStatus === "interpreter" &&
+                                <div className="interpreterCertification" style={{display:"flex", flexDirection: "column"}}>
                                     <h1>Have a certification?</h1>
-                                    <input id="certification" onChange={(e)=>{this.setState({certification: e.target.value})}} />
-                                    <label for="certification" style={{display: "block"}}>enter here</label>
+                                    <input type="radio" name="certification" value="#"  onChange={(e)=>{this.setState({userStatus: e.target.value})}}/>
+                                    <label for="interpreter">#</label>
+                                    <input type="radio" name="certification" value="#"  onChange={(e)=>{this.setState({userStatus: e.target.value})}}/>
+                                    <label for="#">#</label>
+                                    <input id="certification"  type="text" onChange={(e)=>{this.setState({certification: e.target.value})}} />
+                                    <label for="certification" style={{display: "block"}}>Enter alternative certification title here.</label>
                                 </div>
                             }
                         </div>
